c95a7a6f3ecd94688e450579c8dedf8b21e4ae3c
Carlos <kadukitesesi@gmail.com>
2024-09-26 13:09:36 -0300

    Mudança segurança

--- src/main/java/com/kadukitesesi/escalatrabalho/config/SecurityConfigurations.java
+++ src/main/java/com/kadukitesesi/escalatrabalho/config/SecurityConfigurations.java
@@ -30,7 +30,7 @@ public class SecurityConfigurations {
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                    .anyRequest().authenticated()
+                    .anyRequest().permitAll()
                 )
         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
